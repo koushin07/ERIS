bd08b447b45df4bb3be8b9e840e4e9eb5c7dc0b0
koushin07 <canaresmiko3@gmail.com>
2023-01-29 10:13:54 +0800

    sett

--- routes/auth.php
+++ routes/auth.php
@@ -13,11 +13,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //     ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])
-        ->name('register');
+    // Route::post('register', [RegisteredUserController::class, 'store'])
+    //     ->name('register');
     Route::post('register_municipality', [RegisteredUserController::class, 'store_municipality'])
      
         ->name('municipalityRegistration');
