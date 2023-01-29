2044a53cbb4edd55cb270bb722e0c7b3ea34ca01
koushin07 <43770631+koushin07@users.noreply.github.com>
2023-01-29 17:56:02 +0800

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,8 @@
 echo "Running composer"
+RUN composer init --name=myvendor/mypackage --type=project
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-composer update
+
 
 echo "npm install"
 npm install 
